681099fb8e8b884d78221c95be172019a2f21990
System Administrator <root@yazdans-MacBook-Pro.local>
2020-10-10 17:11:15 +0330

    Updates

---
+++ precache-manifest.b1215860cd01475d9ca61a9eae388572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780ea79a7bf7619fbf63ae3287e59783",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6f0ef3ca60b131f06a40",
+    "url": "/monsters-rolodex/static/css/main.2f0d33fb.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0ef3ca60b131f06a40",
+    "url": "/monsters-rolodex/static/js/main.9e7602b2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
